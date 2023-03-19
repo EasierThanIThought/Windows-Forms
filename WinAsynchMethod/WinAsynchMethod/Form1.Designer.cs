b79921dd48b10709f1d7f3d2ae2f86f91d5592da
Ekaterina Buldakova <91080991+EasierThanIThought@users.noreply.github.com>
2023-03-19 12:14:55 +0300

    Add files via upload

---
+++ WinAsynchMethod/WinAsynchMethod/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace WinAsynchMethod
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.btnWork = new System.Windows.Forms.Button();
+            this.lblA = new System.Windows.Forms.Label();
+            this.lblB = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.txbA = new System.Windows.Forms.TextBox();
+            this.txbB = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.helpProvider1 = new System.Windows.Forms.HelpProvider();
+            this.help = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // btnRun
+            // 
+            this.helpProvider1.SetHelpString(this.btnRun, "Sum");
+            this.btnRun.Location = new System.Drawing.Point(6, 51);
+            this.btnRun.Name = "btnRun";
+            this.helpProvider1.SetShowHelp(this.btnRun, true);
+            this.btnRun.Size = new System.Drawing.Size(94, 34);
+            this.btnRun.TabIndex = 0;
+            this.btnRun.Text = "Сумма";
+            this.toolTip1.SetToolTip(this.btnRun, "Sum");
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // btnWork
+            // 
+            this.helpProvider1.SetHelpString(this.btnWork, "Start work");
+            this.btnWork.Location = new System.Drawing.Point(281, 197);
+            this.btnWork.Name = "btnWork";
+            this.helpProvider1.SetShowHelp(this.btnWork, true);
+            this.btnWork.Size = new System.Drawing.Size(96, 35);
+            this.btnWork.TabIndex = 1;
+            this.btnWork.Text = "Работа";
+            this.toolTip1.SetToolTip(this.btnWork, "Start work");
+            this.btnWork.UseVisualStyleBackColor = true;
+            this.btnWork.Click += new System.EventHandler(this.btnWork_Click);
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Location = new System.Drawing.Point(2, 19);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(98, 20);
+            this.lblA.TabIndex = 2;
+            this.lblA.Text = "Значение А";
+            // 
+            // lblB
+            // 
+            this.lblB.AutoSize = true;
+            this.lblB.Location = new System.Drawing.Point(197, 19);
+            this.lblB.Name = "lblB";
+            this.lblB.Size = new System.Drawing.Size(98, 20);
+            this.lblB.TabIndex = 3;
+            this.lblB.Text = "Значение В";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(106, 58);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 20);
+            this.lblResult.TabIndex = 4;
+            // 
+            // txbA
+            // 
+            this.helpProvider1.SetHelpString(this.txbA, "For input integer A");
+            this.txbA.Location = new System.Drawing.Point(106, 13);
+            this.txbA.Name = "txbA";
+            this.helpProvider1.SetShowHelp(this.txbA, true);
+            this.txbA.Size = new System.Drawing.Size(76, 26);
+            this.txbA.TabIndex = 5;
+            this.toolTip1.SetToolTip(this.txbA, "For input integer A");
+            // 
+            // txbB
+            // 
+            this.helpProvider1.SetHelpString(this.txbB, "For input integer B");
+            this.txbB.Location = new System.Drawing.Point(301, 13);
+            this.txbB.Name = "txbB";
+            this.helpProvider1.SetShowHelp(this.txbB, true);
+            this.txbB.Size = new System.Drawing.Size(76, 26);
+            this.txbB.TabIndex = 6;
+            this.toolTip1.SetToolTip(this.txbB, "For input integer B");
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 91);
+            this.button1.Name = "button1";
+            this.helpProvider1.SetShowHelp(this.button1, false);
+            this.button1.Size = new System.Drawing.Size(94, 35);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Разность";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(106, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 9;
+            // 
+            // helpProvider1
+            // 
+            this.helpProvider1.HelpNamespace = "D:\\учеба, итмо\\4 год\\Создание ПО\\R1\\WinAsynchMethod\\WinAsynchMethod\\TextFile1.txt" +
+    "";
+            // 
+            // help
+            // 
+            this.help.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.help.Location = new System.Drawing.Point(13, 197);
+            this.help.Name = "help";
+            this.help.Size = new System.Drawing.Size(75, 35);
+            this.help.TabIndex = 10;
+            this.help.Text = "HELP!";
+            this.help.UseVisualStyleBackColor = false;
+            this.help.Click += new System.EventHandler(this.help_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 244);
+            this.Controls.Add(this.help);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txbB);
+            this.Controls.Add(this.txbA);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lblB);
+            this.Controls.Add(this.lblA);
+            this.Controls.Add(this.btnWork);
+            this.Controls.Add(this.btnRun);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.HelpButton = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Асинхронный запуск";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Button btnWork;
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.TextBox txbA;
+        private System.Windows.Forms.TextBox txbB;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.HelpProvider helpProvider1;
+        private System.Windows.Forms.Button help;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
